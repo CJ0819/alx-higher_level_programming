f48352ce222a71c32f3925a799065f0c19f60019
Joseph Conduah <josephconduah02@gmail.com>
2023-11-27 20:01:49 +0000

    finished

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,12 @@
+def magic_calculation(a, b):
+  """
+  This function performs the same calculation as the given Python bytecode.
+
+  Args:
+    a: The first operand.
+    b: The second operand.
+
+  Returns:
+    The result of the calculation.
+  """
+  return 98 + a ** b
