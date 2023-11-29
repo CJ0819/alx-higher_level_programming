a14089dd20d988a17e07e46a19ca23259948dffb
Joseph Conduah <josephconduah02@gmail.com>
2023-11-29 01:49:34 +0000

    2-print_alphabet.py

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Loop through ASCII values for lowercase alphabet and print each character
+for i in range(ord('a'), ord('z') + 1):
+    print(chr(i), end='')
+
+# Print a new line at the end
+print()
